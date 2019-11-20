27093a4e2246e77b623509c35c81b4cc30a64733
Luca Bucher <lucbu01@bluewin.ch>
2019-11-20 08:52:11 +0100

    Add NewEntry window

---
+++ MoneyViewerPro/NewEntry.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoneyViewerPro
+{
+    /// <summary>
+    /// Interaktionslogik für NewEntry.xaml
+    /// </summary>
+    public partial class NewEntry : Window
+    {
+        public NewEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
