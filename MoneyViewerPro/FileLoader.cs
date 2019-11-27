f64ead11948b63dd0b4d0bd7546b927fa9603fd8
Lars Ghezzi <lars_ghezzi@sluz.ch>
2019-11-27 09:01:48 +0100

    write and load file without password

---
+++ MoneyViewerPro/FileLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MoneyViewerPro
+{
+    class FileLoader
+    {
+        public static EntryList loadEntries(string path)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(EntryList));
+            string data = File.ReadAllText(path);
+            using (TextReader tr = new StringReader(data))
+            {
+                EntryList deserialized = (EntryList)deserializer.Deserialize(tr);
+                return deserialized;
+            }
+        }
+
+        public static CategoryList loadCategory(string path)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(CategoryList));
+            string data = File.ReadAllText(path);
+            using (TextReader tr = new StringReader(data))
+            {
+                CategoryList deserialized = (CategoryList)deserializer.Deserialize(tr);
+                return deserialized;
+            }
+        }
+    }
+}
---
+++ MoneyViewerPro/FileWriter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MoneyViewerPro
+{
+    class FileWriter
+    {
+        public static void write(string path, EntryList entries) {
+            string serializedData = string.Empty;
+
+            XmlSerializer serializer = new XmlSerializer(entries.GetType());
+            using (StringWriter sw = new StringWriter())
+            {
+                serializer.Serialize(sw, entries);
+                serializedData = sw.ToString();
+            }
+
+            File.WriteAllText(path, serializedData);
+        }
+
+        public static void write(string path, CategoryList entries)
+        {
+            string serializedData = string.Empty;
+
+            XmlSerializer serializer = new XmlSerializer(entries.GetType());
+            using (StringWriter sw = new StringWriter())
+            {
+                serializer.Serialize(sw, entries);
+                serializedData = sw.ToString();
+            }
+
+            File.WriteAllText(path, serializedData);
+        }
+    }
+}
